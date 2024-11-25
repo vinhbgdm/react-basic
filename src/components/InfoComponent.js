eb002e2516e1c58cfd6dc1b23046b260fc8ce992
vinhbgdm <ninhdinhvinh11012001@gmail.com>
2024-11-25 11:15:11 +0700

    stateless

--- src/components/InfoComponent.js
+++ src/components/InfoComponent.js
@@ -2,43 +2,68 @@ import React from "react";
 import "../styles/DisplayInfo.scss"
 import logo from "../views/logo.svg"
 
-class InfoComponent extends React.Component{
-    state = {
-        isShowListUser: true
-    }
-    handleShowHide = () => {
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        })
-    }
-    render(){
-        let {name, age, address, arrJobs} = this.props
-        console.log(arrJobs)
-        return(
-            <div className="display-info">
-                ---------------------------------------------------------------------------------------------------------------------------<br/>
-                <img src={logo} alt=""/>
-                <div>
-                    <input type="submit" value={this.state.isShowListUser ? "Hide list user" : "Show list user"} onClick={() => {this.handleShowHide()}} />  
-                </div>
+// class InfoComponent extends React.Component{
+//     state = {
+//         isShowListUser: true
+//     }
+//     handleShowHide = () => {
+//         this.setState({
+//             isShowListUser: !this.state.isShowListUser
+//         })
+//     }
+//     render(){
+//         let {name, age, address, arrJobs} = this.props
+//         console.log(arrJobs)
+//         return(
+//             <div className="display-info">
+//                 ---------------------------------------------------------------------------------------------------------------------------<br/>
+//                 <img src={logo} alt=""/>
+//                 <div>
+//                     <input type="submit" value={this.state.isShowListUser ? "Hide list user" : "Show list user"} onClick={() => {this.handleShowHide()}} />  
+//                 </div>
 
-                { this.state.isShowListUser &&
-                <div className="jobsList">
-                    Infor component: {name} - {age} - {address}
-                    {
-                        arrJobs.map((item, index) => {
-                            return (
-                                <div key={item.id} className={+item.salary > 600 ? "green" : "red"}>
-                                    {item.title} - {item.salary}
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+//                 { this.state.isShowListUser &&
+//                 <div className="jobsList">
+//                     Infor component: {name} - {age} - {address}
+//                     {
+//                         arrJobs.map((item, index) => {
+//                             return (
+//                                 <div key={item.id} className={+item.salary > 600 ? "green" : "red"}>
+//                                     {item.title} - {item.salary}
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
+//                 }
+//             </div>
+//         )
+//     }
+// }
+
+const InfoComponent = (props) => {
+    let {name, age, address, arrJobs} = props
+    return(
+        <div className="display-info">
+            ---------------------------------------------------------------------------------------------------------------------------<br/>
+            <img src={logo} alt=""/>
+
+            { true &&
+            <div className="jobsList">
+                Infor component: {name} - {age} - {address}
+                {
+                    arrJobs.map((item, index) => {
+                        return (
+                            <div key={item.id} className={+item.salary > 600 ? "green" : "red"}>
+                                {item.title} - {item.salary}
+                            </div>
+                        )
+                    })
                 }
             </div>
-        )
-    }
+            }
+        </div>
+    )
 }
 
 export default InfoComponent;
